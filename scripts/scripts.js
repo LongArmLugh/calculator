ca04518a0fc8632ec03b07b68ec0c086d6840c2f
Lugh_Lámfada <healingphases@gmail.com>
2021-12-14 15:49:29 -0800

    added: model, '=' sign logic, everything updtes view correctly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,3 @@
-// calc functions
 // add 
 function add(a, b) {
     return +a + +b;
@@ -21,10 +20,8 @@ function operate(operation, a, b) {
     operation(a, b);
 }
 
-// const screenOut = document.getElementById('screenTxt');
-
-// Create an object:
 const view = {
+    // View Object
     screen: null,
     output: null,
     update: function(output) {
@@ -36,8 +33,108 @@ const view = {
         this.screen.textContent = this.output;
     }
 }
-
 view.screen = document.getElementById('screenTxt');
+
+const operator = {
+    // Operator object
+    atTotal: false,
+    isActive: false,
+    list: null,
+    type: null,
+    hasPeriod: false,
+    activate: function() {
+        if (this.isActive === false) {
+            this.isActive = true;
+        }
+    },
+    press: function() {
+        if (this.type === '=') {
+            this.atTotal = true;
+            this.list.splice(0, this.list.length);
+            this.list.push('total');
+        } else if (this.isActive === false) {
+            this.list.push(this.type);
+            this.activate();
+        } 
+    },
+}
+// Input handling
+// perhaps turn into an object that can detect state
+// operators and period should toggle state to disable
+// numbers should toggle state to enable operators.
+const model = {
+    // Model Object
+    inputs: [],
+    reset: function() {
+        this.inputs.splice(0, this.inputs.length);
+    }, 
+    parse: function() {
+        console.log('parsing');
+    },
+}
+
+
+operator.list = model.inputs;
+
+const addNumberEvent = function() {
+    if (operator.atTotal === true) {
+        operator.atTotal = false;
+        model.reset();
+        view.reset();
+    }
+    const btn = this.textContent;
+    operator.isActive = false;
+    model.inputs.push(btn);
+    view.update(model.inputs.join(''));
+};
+
+const addOperatorEvent = function() {
+    const btn = this.textContent;  
+    switch (btn) { 
+        case 'c':
+            model.inputs.splice(0, model.inputs.length);
+            view.reset();
+            break;
+        case '+/-':
+            break;
+        case '%':
+            break;
+        case '/':
+            operator.type = '+';
+            operator.press();
+            break;
+        case 'x':
+            operator.type = '+';
+            operator.press();
+            break;
+        case '-':
+            operator.type = '+';
+            operator.press();
+            break;
+        case '+':
+            operator.type = '+';
+            operator.press();
+            break;
+        case '.':
+            operator.type = '+';
+            operator.press();
+            break;
+        case '=':
+            operator.type = '=';
+            operator.press();
+            break;
+    }
+    view.update(model.inputs.join(''));
+}
+
+const btnList = Array.from(document.querySelectorAll('.btn:not(.operator)'));
+const operatorList = Array.from(document.querySelectorAll('.operator'));
+btnList.forEach(item => {
+    item.addEventListener('click', addNumberEvent);
+});
+operatorList.forEach(item => {
+    item.addEventListener('click', addOperatorEvent);
+});
 
 
 
